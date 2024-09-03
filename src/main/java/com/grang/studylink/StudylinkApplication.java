b4aafebf1d16f55d602d719e2e390d4b3e374682
Leeforgiveness <grang0224@g.kmou.ac.kr>
2024-09-03 17:16:33 +0900

    init

---
+++ src/main/java/com/grang/studylink/StudylinkApplication.java
@@ -0,0 +1,13 @@
+package com.grang.studylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudylinkApplication.class, args);
+	}
+
+}
