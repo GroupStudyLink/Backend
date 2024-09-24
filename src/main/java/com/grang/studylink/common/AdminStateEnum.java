056ac26aa11beffda11fe99938bb88d4e10ac4dc
Leeforgiveness <grang0224@g.kmou.ac.kr>
2024-09-24 16:42:26 +0900

    feat: StudyMember중간테이블에 어드인인지 아닌지 확인하는 enum추가

---
+++ src/main/java/com/grang/studylink/common/AdminStateEnum.java
@@ -0,0 +1,12 @@
+package com.grang.studylink.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AdminStateEnum {
+
+    Member,
+    Admin,
+}
