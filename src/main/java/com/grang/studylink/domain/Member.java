eaedc3a0a62e72d94d9363441f2d3303633720d3
Leeforgiveness <grang0224@g.kmou.ac.kr>
2024-09-24 16:41:52 +0900

    feat: member엔티티에 password추가

--- src/main/java/com/grang/studylink/domain/Member.java
+++ src/main/java/com/grang/studylink/domain/Member.java
@@ -17,6 +17,8 @@ public class Member {
     private Long id;
     @Column(name = "member_email", nullable = false, length = 30)
     private String email;
+    @Column(name = "member_password", nullable = false, length = 60)
+    private String password;
     @Column(name = "member_uuid", nullable = false, length = 50)
     private String uuid;
     @Column(name = "member_name", nullable = false, length = 10)
@@ -25,9 +27,10 @@ public class Member {
     private String nickName;
 
     @Builder
-    public Member(Long id, String email, String uuid, String name, String nickName) {
+    public Member(Long id, String email, String password, String uuid, String name, String nickName) {
         this.id = id;
         this.email = email;
+        this.password = password;
         this.uuid = uuid;
         this.name = name;
         this.nickName = nickName;
